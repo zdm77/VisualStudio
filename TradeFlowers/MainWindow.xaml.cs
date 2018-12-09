b3519465d866e7dfd926918873efab54876a9642
dzaferman <zaferman@btlab.ru>
2018-12-09 10:09:59 +0300

    Пробую Entity Framework

--- TradeFlowers/MainWindow.xaml.cs
+++ TradeFlowers/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using Npgsql;
+using System.Collections.Generic;
 
 using TradeFlowers.View;
 namespace TradeFlowers
@@ -21,13 +15,16 @@ namespace TradeFlowers
     /// </summary>
     public partial class MainWindow : Window
     {
+        static private string _connectionString;
+        static public string ConnectionString { get=>_connectionString; set=>_connectionString=value; }
         public MainWindow()
         {
             InitializeComponent();
+            ConnectionString= ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
             //Product p = new Product();
             //p.Name = "Тест";
             //MessageBox.Show(p.Name);
-            
+
 
         }
         private void NewClick(object sender, RoutedEventArgs e)
