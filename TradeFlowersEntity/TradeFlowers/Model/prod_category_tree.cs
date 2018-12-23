9611120b303e5b881b06963ad8e44e24fb22df05
zdm <zdm77@mail.ru>
2018-12-23 15:55:21 +0300

    Работающее дерево

---
+++ TradeFlowersEntity/TradeFlowers/Model/prod_category_tree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace TradeFlowers.Model
+{
+    public class Record
+    {
+        public int TreeId { get; set; }
+
+        public int ParentId { get; set; }
+
+        public string Name { get; set; }
+        public string ParentName { get; set; }
+    }
+    class prod_category_tree: prod_category
+    {
+        public List<prod_category_tree> Children { get; set; }
+        public prod_category_tree(string name, int id, int pid)
+        {
+            category_id = id;
+            category_name = name;
+            Children = new List<prod_category_tree>();
+            paerent_category = pid;
+          //  ParentName = pName;
+        }
+        public void Add(Record record)
+        {
+            Traverse().First(node => node.category_id == record.ParentId).Children.Add(new prod_category_tree(record.Name, record.TreeId, record.ParentId));
+        }
+        private IEnumerable<prod_category_tree> Traverse()
+        {
+            yield return this;
+            foreach (prod_category_tree child in Children.SelectMany(node => node.Traverse()))
+            {
+                yield return child;
+            }
+        }
+        public static prod_category_tree CreateTree(List<Record> records)
+        {
+            Record rootRecord = records.First(r => r.ParentId == 0);
+            var root = new prod_category_tree(rootRecord.Name, rootRecord.TreeId, rootRecord.ParentId);
+            foreach (Record record in records.Where(r => r.ParentId != 0))
+            {
+                root.Add(record);
+            }
+            return root;
+        }
+    }
+}
---
+++ TradeFlowersEntity/TradeFlowers/Pages/Dictonary/CategorysView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TradeFlowers.Model;
+
+namespace TradeFlowers.Pages.Dictonary
+{
+    /// <summary>
+    /// Логика взаимодействия для CategorysView.xaml
+    /// </summary>
+    public partial class CategorysView : UserControl
+    {
+        private List<Record> categoryList = new List<Record>();
+        public CategorysView()
+        {
+            InitializeComponent();
+            using (tradeEntities db = new tradeEntities())
+            {
+                foreach (prod_category c in db.prod_category)
+                {
+                    categoryList.Add(
+                        new Record
+                        {
+                            Name = c.category_name,
+                            TreeId = c.category_id,
+                            ParentId = (int)c.paerent_category,
+                        }
+                        );
+                }
+                var root = prod_category_tree.CreateTree(categoryList);
+                tree.ItemsSource = new[] { root };
+            }
+
+        }
+    }
+}
